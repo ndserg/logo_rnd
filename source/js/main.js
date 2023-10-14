7860e319aedaccab90d760e80da261be6790f369
Denis <ndserg@gmail.com>
2023-10-14 19:07:47 +0300

    Изменит структуру проекта. Разобьет HTML и упорядочит стили.

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,9 @@
-import initMenuPopup from './menu';
-import initSlider from './slider';
-import initForm from './contact-form';
+import initWebpSupport from './webp-support.js';
+import initMenuPopup from './menu.js';
+import initForm from './contact-form.js';
 
 window.addEventListener('DOMContentLoaded', () => {
+  initWebpSupport();
   initMenuPopup();
-  initSlider();
   initForm();
 });
